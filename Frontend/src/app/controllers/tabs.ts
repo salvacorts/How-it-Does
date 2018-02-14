33db9d305c5d93a6c624c7e13b1b8fadff9039d2
Salva Corts <salvacortsit@gmail.com>
2018-02-14 10:11:53 +0100

    import error correction

--- Frontend/src/app/controllers/tabs.ts
+++ Frontend/src/app/controllers/tabs.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { ReviewsService, CardKind } from '../services/reviewsService';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
    selector: 'tabs',
