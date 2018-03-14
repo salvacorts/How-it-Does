4d8bc6f2309c7328f338b9e1b3f62e201e500feb
Salva Corts <salvacortsit@gmail.com>
2018-03-14 23:57:12 +0100

    Lets see if it works https://stackoverflow.com/questions/46960446/push-to-google-container-registry-fails-retrying

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -11,6 +11,7 @@ echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ./Backend/creds.json
 gcloud auth activate-service-account $GCLOUD_EMAIL --key-file ./Backend/creds.json
 gcloud --quiet config set project $PROJECT_NAME
 gcloud --quiet config set compute/zone $CLOUDSDK_COMPUTE_ZONE
+docker-credential-gcr configure-docker --token-source="gcloud"
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
 
 
