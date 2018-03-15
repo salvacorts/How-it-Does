b0d88059c577d13c4b1a2e87b9a3d6f8d29cc160
Salva Corts <salvacortsit@gmail.com>
2018-03-15 09:16:05 +0100

    misunderstood with rolling-update

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -14,7 +14,7 @@ gcloud --quiet config set compute/zone $CLOUDSDK_COMPUTE_ZONE
 
 # Configure Docker to access Google Container Registry
 echo $GCR_SERVICE_KEY | base64 --decode -i > ./gcr-creds.json
-docker login -u _json_key -p "$(cat ./gcr-creds.json)" https://gcr.io
+docker login -u _json_key --password-stdin "$(cat ./gcr-creds.json)" https://gcr.io
 
 # Install Kubectl and authenticate
 gcloud components install kubectl
@@ -24,10 +24,10 @@ gcloud container clusters get-credentials main-cluster
 cd Backend/
 docker build -f dockerfile -t gcr.io/how-it-does/backend:latest .
 docker push gcr.io/how-it-does/backend:latest
-kubectl rolling-update backend --image=gcr.io/how-it-does/backend:latest
+kubectl set image deployment/how-it-does backend=backend:latest
 
 # Move from backent to Frontend
 cd ../Frontend/
 docker build -f dockerfile -t gcr.io/how-it-does/frontend:latest .
 docker push gcr.io/how-it-does/frontend:latest
-kubectl rolling-update frontend --image=gcr.io/how-it-does/frontend:latest
+kubectl set image deployment/how-it-does frontend=frontend:latest
