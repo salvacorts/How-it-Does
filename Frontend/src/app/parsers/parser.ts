24d513083a4e8bb12e0ffa9225fdaaa128baf148
Salva Corts <salvacortsit@gmail.com>
2018-03-15 11:48:39 +0100

    changed apiURL

--- Frontend/src/app/parsers/parser.ts
+++ Frontend/src/app/parsers/parser.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 
 /** Base API URL */
-const apiURL = "http://127.0.0.1:8080"
+const apiURL = "http://http://35.202.235.70:8080"
 
 /** 
  * Parser class. It might be extended for custom parser that doesnt require an API 
